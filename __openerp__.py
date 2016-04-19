195f4273c38ee5a2446ae17b48d24624085854ed
khadijaabourkia <abourkiakhadija@hotmail.com>
2016-04-19 10:16:23 +0100

    Update __openerp__.py

--- __openerp__.py
+++ __openerp__.py
@@ -1,3 +1,25 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
+#    $Id$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 {
     'name': 'Base Candidat',
     'version': '1.0',
@@ -10,21 +32,8 @@ Gérer les candidats
 Ce module vous permet d'avoir une base de candidats.
     """
     'author': 'Khadija Abourkia',
-    'website': '',
-    'depends': [
-        'decimal_precision',
-        'hr',
-		'hr_recruitment',
-        'survey',
-        'calendar',
-        'fetchmail',
-        'web_kanban_gauge',
-    ],
     'data': [
-       'hr_base_candidat_view.xml',
-       'hr_recruitment_menu.xml',
-       'views/hr_recruitment.xml',
-
+       'base_candidat_view.xml',
     ],
     'installable': True,
     'auto_install': False,
